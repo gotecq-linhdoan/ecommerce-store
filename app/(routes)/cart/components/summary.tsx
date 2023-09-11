8c1a7d89223a9666973b7bdcf24f39033b1946b7
Tuan Linh <linh.doan@gotecq.com>
2023-09-11 11:20:22 +0700

    fix/billboard-bug

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -8,6 +8,7 @@ import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
+import { useOrigin } from "@/hooks/use-origin";
 
 const Summary = () => {
   const searchParams = useSearchParams();
@@ -29,15 +30,18 @@ const Summary = () => {
     return total + Number(item.price)
   }, 0);
 
+  const origin = useOrigin();
+
   const onCheckout = async () => {
     const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
-      productIds: items.map((item) => item.id)
+      productIds: items.map((item) => item.id),
+      storeUrl: origin,
     });
 
     window.location = response.data.url;
   }
 
-  return ( 
+  return (
     <div
       className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
     >
@@ -47,7 +51,7 @@ const Summary = () => {
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <div className="text-base font-medium text-gray-900">Order total</div>
-         <Currency value={totalPrice} />
+          <Currency value={totalPrice} />
         </div>
       </div>
       <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
@@ -56,5 +60,5 @@ const Summary = () => {
     </div>
   );
 }
- 
+
 export default Summary;
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import getBillboard from "@/actions/get-billboard";
+import getBillboards from "@/actions/get-billboards";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
@@ -8,13 +9,13 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("2c980a34-6eb8-466a-904a-41c55ae91121");
+  const billboards = await getBillboards();
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard 
-          data={billboard}
+          data={billboards[0]}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
